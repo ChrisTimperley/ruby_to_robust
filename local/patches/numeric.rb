83b97f569fc3609ad234e0201af5d3eba71bf378
ChrisTimperley <christimperley@googlemail.com>
2013-12-03 08:48:29 +0000

    Changed Numeric patch to use new patch structure.

--- local/patches/numeric.rb
+++ local/patches/numeric.rb
@@ -1,16 +1,10 @@
-class Numeric
+#  Ensures that integer division returns 0 if the denominator is zero.
+RubyToRobust::Local.register_patch(Numeric, :div, :__div) do |other|
+  other.zero? ? 0 : __div(other)
+end
 
-  # Ensures that integer division returns 0 if the denominator is zero.
-  alias_method :__div, :div
-  def div(other)
-    other.zero? ? 0 : __div(other)
-  end
-
-  # Ensures that the modulus operator returns 0 if a division by zero
-  # error occurs.
-  alias_method :__divmod, :divmod
-  def divmod(other)
-    other.zero? ? 0 : __divmod(other)
-  end
-
+# Ensures that the modulus operator returns 0 if a division by zero
+# error occurs.
+RubyToRobust::Local.register_patch(Numeric, :divmod, :__divmod) do |other|
+  other.zero? ? 0 : __divmod(other)
 end
