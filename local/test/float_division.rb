b376ae99ec9263b91454d2efa6143c1ff653ab77
ChrisTimperley <christimperley@googlemail.com>
2013-12-03 10:32:31 +0000

    Fixed Float Division unit test.

--- local/test/float_division.rb
+++ local/test/float_division.rb
@@ -9,14 +9,14 @@ class TestFloatDivision < Test::Unit::TestCase
     assert_equal(0.5, program[1.0, 2.0])
   end
  
-  def test_hard_dbz_error
+  def test_hard_dbz
     program = lambda { |x, y| x / y }
-    assert_raise(ZeroDivisionError) { program[1.0, 0.0] }
+    assert_equal(Float::INFINITY, program[1.0, 0.0])
   end
 
   def test_soft_dbz
     program = lambda { |x, y| x / y }
-    assert_equal(0.0, program[1.0, 0.0])
+    assert_equal(0.0, RubyToRobust::Local.execute(program, [1.0, 0.0]))
   end
  
 end
