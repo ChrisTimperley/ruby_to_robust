4a16e6fb5015678f68e5e9818cbe27390748487b
ChrisTimperley <christimperley@googlemail.com>
2013-12-03 08:46:03 +0000

    Changed Float patch to use new patch structure.

--- local/patches/float.rb
+++ local/patches/float.rb
@@ -1,17 +1,10 @@
-class Float
+# Ensures that any float divided by zero gives 0.0
+RubyToRobust::Local.register_patch(Fixnum, :/, :__fdiv) do |other|
+  other.zero? ? 0.0 : __fdiv(other)
+end
 
-  # Ensures that any float divided by zero gives 0.0
-  alias_method :__fdiv, :/
-  def /(other)
-    other.zero? ? 0.0 : __fdiv(other)
-  end
-  
-  # Ensures that the modulus operator returns zero if a divide
-  # by zero error would occur.
-  alias_method :__modulo, :modulo
-  def %(other)
-    other.zero? ? 0 : __modulo(other)
-  end
-  alias_method :modulo, :%
-
+# Ensures that the modulus operator returns zero if a divide
+# by zero error would occur.
+RubyToRobust::Local.register_patch(Float, :%, :__mod) do |other|
+  other.zero? ? 0 : __mod(other)
 end
