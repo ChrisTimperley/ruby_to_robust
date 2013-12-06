5abad229acd6e4c4888f89c51d49640ab46b2c13
ChrisTimperley <christimperley@googlemail.com>
2013-12-06 22:05:08 +0000

    Experimented with idea of using instance eval to dynamically generate anonymous functions with error reporting information.

--- global/test/eval.rb
+++ global/test/eval.rb
@@ -1,14 +1,21 @@
 # This is AWESOME!
 # Going to vastly improve performance!
 # We can tell Ruby a file name and line number to report using.
-class Klass
-  class_eval <<-M, "I_AM_AN_EVAL", 0
-    def div(x,y)
+class CleverLambda
+  instance_eval <<-M, "I_AM_AN_EVAL", 0
+    def call(x,y)
       z = "test!"
       return x/y
     end
   M
 end
 
-o = Klass.new
-puts o.div(3,0)
+
+o = CleverLambda.new
+code = "def call(x,y)
+  z = 'test!'
+  return x/y
+end"
+o.instance_eval(code, "I_AM_A_CLEVER_LAMBDA", 0)
+
+puts o.call(3,0)
