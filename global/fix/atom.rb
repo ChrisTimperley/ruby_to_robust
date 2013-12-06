e6c7dcc3d3d03da4841377794b5ab6b176c961ed
ChrisTimperley <christimperley@googlemail.com>
2013-12-06 14:50:11 +0000

    Implemented idea of atomic fix history and adjusted line number calculation.

--- global/fix/atom.rb
+++ global/fix/atom.rb
@@ -2,6 +2,7 @@
 # may be represented as a collection of such atomic fixes.
 class RubyToRobust::Global::Fix::Atom
 
+  # The line which this atomic fix changes.
   attr_reader :line_no
 
   # Constructs a new fix atom.
@@ -12,20 +13,34 @@ class RubyToRobust::Global::Fix::Atom
     @line_no = line_no
   end
 
-  # Destructively applies the change described by this atomic fix to the given source code.
+  # Calculates the number of lines that will be added by this fix.
+  def lines_added
+    raise NotImplementedError, 'No "lines_added" method was implemented by this Atom.'
+  end
+
+  # Calculate the line number in the adjusted source code that corresponds to the specified
+  # line in the original source code.
   #
-  # Rather than destructively operating on the source code we could return a modified copy,
-  # but since we never need to operate on a new copy we save computational resources by
-  # operating on the input source code directly. If you really wanted to create a new copy
-  # then you could add a new method "apply" which simply passed a clone of its input to
-  # the "apply!" method.
+  # *Parameters:*
+  # * changes, an array of the atomic changes already applied to this source.
+  def adjusted_line_no(changes)
+    adj_n = @line_no
+    changes.each do |c|
+      if c.line_no < @line_no and lines_added < 0
+        adj_n += c.lines_added
+      elsif c.line_no >= line_no and lines_added > 0
+        adj_n += c.lines_added
+      end
+    end
+    return adj_n
+  end
+
+  # Destructively applies the change described by this atomic fix to the given source code.
   #
   # *Parameters:*
   # * source, the source code of the affected method (represented as a sequence of lines).
-  #
-  # *Returns:*
-  # The modified source code.
-  def apply!(source)
+  # * changes, an array of the atomic changes already applied to this source (in order of application).
+  def apply!(source, changes)
     raise NotImplementedError, 'No "apply!" method was implemented by this Atom.'
   end
 
