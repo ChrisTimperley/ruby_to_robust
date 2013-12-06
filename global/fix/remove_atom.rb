efbdda8744f5aa57118f6bf0f2dd1234a6f64b7c
ChrisTimperley <christimperley@googlemail.com>
2013-12-06 14:50:48 +0000

    Implemented atomic fix for line removals.

--- global/fix/remove_atom.rb
+++ global/fix/remove_atom.rb
@@ -1,9 +1,17 @@
 # A remove atom is used to remove a line from the affected source code.
 class Wallace::Global::Fix::RemoveAtom < Wallace::Global::Fix::Atom
 
+  # Removes a single line from the source code.
+  def lines_added; -1; end
+
   # Removes a specific line from the affected source code and returns the result.
-  def apply!(source)
-    
+  #
+  # *Parameters:*
+  # * source, the (partially fixed) affected source.
+  # * changes, an array of the atomic changes already applied to this source (in order of application).
+  def apply!(source, changes)
+    source.delete_at(adjusted_line_no(changes))
+    changes << self
   end
 
 end
