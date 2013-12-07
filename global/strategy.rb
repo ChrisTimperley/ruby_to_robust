33578cdea1c0c5c773a3185a7aa8b4aad3a1e76b
ChrisTimperley <christimperley@googlemail.com>
2013-12-07 12:02:34 +0000

    Added '#extract_calls' method to Strategy class.

--- global/strategy.rb
+++ global/strategy.rb
@@ -2,6 +2,56 @@
 # within a given function.
 class RubyToRobust::Global::Strategy
 
+  # Extracts the co-ordinates of all method calls on a given line.
+  #
+  # *Parameters:*
+  # * line, the line to extract method calls from.
+  #
+  # *Returns:*
+  # An array of co-ordinate pairs (each an array - could use a range?).
+  def self.extract_calls(line)
+
+    # Find the co-ordinates of every method call in the string.
+    stack = []
+    calls = []
+    (0..line.length).each do |index|
+
+      # Get the character at this index.
+      char = line[index]
+
+      # If this is the start of a method call,
+      # add the starting index to the stack.
+      if char == '('
+        stack << index
+
+      # If this is the end of a method call,
+      # pop the last index off the stack and combine
+      # it with the current index to give the co-ordinates
+      # of the call.
+      elsif char == ')'
+        calls << [stack.pop, index]
+      end
+
+    end
+
+    # Post-process the co-ordinates so that they start from the label
+    # of the method call rather than the bracket opening. Do this by
+    # moving towards the start of the original string until the method
+    # definition has finished (checked by looking at the character).
+    calls.each_index do |i|
+      start_at = calls[i][0]
+      until start_at == 0 do
+        char = line[start_at-1]
+        break if not (char.match(/^[[:alpha:]]$/) or ['_',':','.'].include? char)
+        start_at -= 1
+      end
+      calls[i][0] = start_at
+    end
+
+    return calls
+    
+  end
+
   # Generates a list of candidate fixes to a given problem.
   #
   # *Parameters:*
@@ -9,7 +59,7 @@ class RubyToRobust::Global::Strategy
   # * error, the error which occurred within the method.
   #
   # *Returns:*
-  # An array of candidate solutions to fix the root of the error.
+  # A (possibly empty) array of candidate solutions to fix the root of the error.
   def generate_candidates(method, error)
     raise NotImplementedError, 'No "generate_candidates" method was implemented by this Strategy.'
   end
