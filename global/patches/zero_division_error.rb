2751176837612ed72f9ecc91cefb8ed8484904c4
ChrisTimperley <christimperley@googlemail.com>
2013-12-09 14:10:51 +0000

    Changed 'ZeroDivisionError.line_no' to use Robust Proc unique identifier to extract relevant line from error backtrace.

--- global/patches/zero_division_error.rb
+++ global/patches/zero_division_error.rb
@@ -4,14 +4,18 @@
 # WARNING: I'm not sure that this will work with the current form of RobustProc!
 class ZeroDivisionError
 
-  # Finds the line that the zero division error was thrown on (in
-  # the monitored function). Without providing a method name, the
-  # backtrace would determine which (unmonitored) method caused
-  # the problem. We want to trap the problem inside the monitored
-  # method instead!
-  def line_no(method_name)
+  # Finds the line that the zero division error was first thrown on by
+  # a given method (not the first line that it occurs at, since this may
+  # be within a non-robust method).
+  #
+  # *Parameters:*
+  # * method, the method to extract the line number for.
+  #
+  # *Returns:*
+  # The line that the error occurred on within the provided method.
+  def line_no(method)
     backtrace.each do |b|
-      return b[/:(\d+):/][1...-1].to_i - 1 if b.end_with? "#{method_name}'"
+      return b[/:(\d+):/][1...-1].to_i if b.start_with?(method.code)
     end
     return nil
   end
