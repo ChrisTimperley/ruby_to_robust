294c4b1b76017b939bac5c5eda988a2c30909b44
ChrisTimperley <christimperley@googlemail.com>
2013-12-10 12:02:59 +0000

    Implemented 'Local#protected' and removed almost all existing code from Local module.

--- local/local.rb
+++ local/local.rb
@@ -13,92 +13,28 @@
 # Author: Chris Timperley
 module RubyToRobust::Local
 
-  # By default the robustness measures are disabled.
-  @enabled = false
-
-  # List of associated contexts for soft-binding.
-  @contexts = []
-
   # List of patches.
   @patches = []
 
-  # Executes a given method (or proc) under local robustness protection.
+  # Executes a given block under local robustness protection.
   #
   # *Parameters:*
-  # * method, the method to execute using local robustness protection.
-  # * params, the parameters to the method call.
+  # * *contexts, depickled list of context objects to protect method calls for.
+  # * &block, the block to execute under local robustness protection.
   #
   # *Returns:*
-  # The result of the method call.
-  def self.execute(method, params)
-
-    # Before calling the method, enable all the attached monkey patches
-    # to use "softer" semantics.
+  # * The result of the block execution.
+  def self.protected(*contexts, &block)
+    patches.each { |p| p.prepare(contexts) }
+    patches.each { |p| p.enable }
+    
     begin
-
-      enable_patches!
-      return method.call(*params)
-
-    # Ensure that all the patches are disabled and the original semantics
-    # are restored before returning the result, even when an exception
-    # might occur.
+      result = block.call
     ensure
-      disable_patches!
+      patches.each { |p| p.disable }
     end
 
-  end
-
-  # Returns true if this robustness measure is enabled, false if not.
-  def self.enabled?
-    @enabled
-  end
-
-  # Returns false if local robustness is disabled.
-  def self.disabled?
-    not enabled?
-  end
-
-  # Prepares the local robustness layer to be used.
-  # Produces the method look-up table for the associated context object and
-  # detaches all the original methods.
-  #
-  # *Parameters:*
-  # * context, the context object to prepare.
-  def self.prepare(context)
-    @contexts << context
-    context.collapse_methods!
-  end
-
-  # Clears the list of associated context objects and restores their method
-  # look-up table into standard methods.
-  def self.clear
-    @contexts.each { |c| c.restore_methods! }
-    @contexts.clear
-  end
-
-  protected
-  
-  # Registers a patch for use with local robustness protection.
-  #
-  # *Parameters:*
-  # * binding, the class or module of the method that should be patched.
-  # * name, the name of the method to be patched.
-  # * backup, the name to store the old method under.
-  # * replacement, the replacement method itself.
-  def self.register_patch(binding, name, backup, &replacement)
-    @patches << RubyToRobust::Local::Patch.new(binding, name, backup, replacement)
-  end
-
-  # Enables all the local robustness patches for soft semantics.
-  def self.enable_patches!
-    @patches.each { |p| p.apply! }
-    @enabled = true
-  end
-
-  # Disables the soft semantic patches and restores standard semantics.
-  def self.disable_patches!
-    @patches.each { |p| p.unapply! }
-    @enabled = false
+    return result
   end
 
 end
