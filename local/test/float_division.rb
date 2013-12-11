bb4d3cc71056d3c143e484abd7d12e2821942958
ChrisTimperley <christimperley@googlemail.com>
2013-12-11 12:30:04 +0000

    Adapted Float Division unit test to match new interface of Local module.

--- local/test/float_division.rb
+++ local/test/float_division.rb
@@ -1,22 +1,28 @@
-require_relative '../local/init.rb'
+require_relative '../init.rb'
 require 'test/unit'
  
 class TestFloatDivision < Test::Unit::TestCase
  
   def test_standard_behaviour
     program = lambda { |x, y| x / y }
-    assert_equal(0.5, RubyToRobust::Local.execute(program, [1.0, 2.0]))
-    assert_equal(0.5, program[1.0, 2.0])
+    assert_equal(5.0, RubyToRobust::Local.protected { program[50.0, 10.0] })
+    assert_equal(5.0, program[50.0, 10.0])
   end
  
   def test_hard_dbz
     program = lambda { |x, y| x / y }
     assert_equal(Float::INFINITY, program[1.0, 0.0])
+    assert_equal(Float::INFINITY, program[2.0, 0.0])
+    assert_equal(Float::INFINITY, program[10.0, 0.0])
+    assert_equal(Float::INFINITY, program[255.0, 0.0])
   end
 
   def test_soft_dbz
     program = lambda { |x, y| x / y }
-    assert_equal(0.0, RubyToRobust::Local.execute(program, [1.0, 0.0]))
+    assert_equal(0.0, RubyToRobust::Local.protected { program[1.0, 0.0] })
+    assert_equal(0.0, RubyToRobust::Local.protected { program[2.0, 0.0] })
+    assert_equal(0.0, RubyToRobust::Local.protected { program[10.0, 0.0] })
+    assert_equal(0.0, RubyToRobust::Local.protected { program[255.0, 0.0] })
   end
  
 end
