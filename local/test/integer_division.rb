2c3d1f9c5daad834079dd2ee7c922563d05f9527
ChrisTimperley <christimperley@googlemail.com>
2013-12-11 12:26:51 +0000

    Adapted integer division unit test to match new interface of Local module.

--- local/test/integer_division.rb
+++ local/test/integer_division.rb
@@ -5,7 +5,7 @@ class TestIntegerDivision < Test::Unit::TestCase
  
   def test_standard_behaviour
     program = lambda { |x, y| x / y }
-    assert_equal(5, RubyToRobust::Local.execute(program, [50, 10]))
+    assert_equal(5, RubyToRobust::Local.protected { program[50, 10] })
     assert_equal(5, program[50, 10])
   end
  
@@ -19,10 +19,10 @@ class TestIntegerDivision < Test::Unit::TestCase
 
   def test_soft_dbz
     program = lambda { |x, y| x / y }
-    assert_equal(0, RubyToRobust::Local.execute(program, [1, 0]))
-    assert_equal(0, RubyToRobust::Local.execute(program, [2, 0]))
-    assert_equal(0, RubyToRobust::Local.execute(program, [10, 0]))
-    assert_equal(0, RubyToRobust::Local.execute(program, [255, 0]))
+    assert_equal(0, RubyToRobust::Local.protected { program[1, 0] })
+    assert_equal(0, RubyToRobust::Local.protected { program[2, 0] })
+    assert_equal(0, RubyToRobust::Local.protected { program[10, 0] })
+    assert_equal(0, RubyToRobust::Local.protected { program[255, 0] })
   end
  
 end
